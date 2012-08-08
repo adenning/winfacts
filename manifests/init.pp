9a86b0876679e48ea7dfa666ab717a18d528f102
Adam Denning <adamd@popstarnetworks.com>
2012-08-08 10:56:25 -0500

    Adding source files

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+class winfacts {
+#  notify{  "computer_model: ${::computer_model}": }
+#  notify{  "monitor_description: ${::monitor_description}": }
+#  notify{  "monitor_id: ${::monitor_id}": }
+#  notify{  "monitor_resolution: ${::monitor_resolution}": }
+#  notify{  "motherboard_manufacturer: ${::motherboard_manufacturer}": }
+#  notify{  "motherboard_product: ${::motherboard_product}": }
+#  notify{  "motherboard_serialnumber: ${::motherboard_serialnumber}": }
+#  notify{  "motherboard_version: ${::motherboard_version}": }   
+#  notify{  "os_architecture: ${::os_architecture}": } 
+#  notify{  "soundcard_description: ${::soundcard_description}": }
+#  notify{  "soundcard_manfacturer: ${::soundcard_manfacturer}": 
+#  notify{  "videocard_currentrefreshrate: ${::videocard_currentrefreshrate}": }
+#  notify{  "videocard_description: ${::videocard_description}": }
+#  notify{  "videocard_driver_version: ${::videocard_driver_version}": }
+#  notify{  "videocard_maxrefreshrate: ${::videocard_maxrefreshrate}": }
+#  notify{  "videocard_memory: ${::videocard_memory}": }    
+}
