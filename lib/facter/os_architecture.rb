3505611cd20ce575d467529abc1539cb70797494
Adam Denning <adam.denning@gmail.com>
2012-12-06 13:34:13 -0600

    fixed os_architecture fact broken in older versions of Windows

--- lib/facter/os_architecture.rb
+++ lib/facter/os_architecture.rb
@@ -4,10 +4,11 @@ Facter.add(:os_architecture) do
     result = ''
     require 'win32ole'
     wmi = WIN32OLE.connect('winmgmts://./root/CIMV2')
-    query = wmi.ExecQuery('select OSArchitecture from Win32_OperatingSystem')
+    query = wmi.ExecQuery('select AddressWidth from Win32_Processor where DeviceID="CPU0"')
     query.each do |os|
-        result = os.OSArchitecture
-        break
+       # This typically returns 32-bit or 64-bit, but I've seen it only return 64 and 32 in some embedded instances.
+       # To keep the fact consistent we only take the first two characters.
+        result = os.AddressWidth.to_s[0..1]
     end
     result
   end
